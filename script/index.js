851920f382694648e78bc0fcdaf87986d7778da2
walter26 <walterrafael26@gmail.com>
2020-07-09 21:38:18 -0600

    Add link tree page

---
+++ script/index.js
@@ -0,0 +1,40 @@
+var slideIndex, slides, timer, isPaused = false
+
+function initGallery() {
+    slides = document.getElementsByClassName("imageHolder")
+    slideIndex = slides.length - 1
+
+    slideChange()
+}
+
+function slideChange() {
+    for (let i = slides.length - 1; i > 0; i--) {
+        slides[i].addEventListener("click", () => { console.log(isPaused = !isPaused) })
+    }
+
+
+    timer = setInterval(() => {
+        if (!isPaused) {
+            slides[slideIndex].style.opacity = 0
+            mapSlideIndex()
+        }
+    }, 5000)
+}
+
+function mapSlideIndex() {
+    slideIndex--
+
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1
+
+        isPaused = true
+
+        for (let i = slides.length - 1; i > 0; i--) {
+            slides[i].style.opacity = 1
+        }
+
+        isPaused = false
+    }
+}
+
+initGallery()
